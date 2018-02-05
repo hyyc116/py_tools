895b9dbb703404f01073d59aff30a99f50d9fc07
hyyc116 <hyyc116@gmail.com>
2018-02-05 15:29:31 +0800

    add

---
+++ del_un_crawled.py
@@ -0,0 +1,27 @@
+#coding:utf-8
+from export_data import *
+
+def del_data():
+    msas = set([line.strip() for line in open('msa_crawled.txt')])
+    counties = set([line.strip() for line in open('county_crawled.txt')])
+    query_op = dbop()
+    del_op = dbop()
+    sql = 'select id,name from msa'
+    count=0
+    for mid,name in query_op.query_database(sql):
+        if name not in msas:
+            del_op.execute_sql('delete from datapiece where msa_id={:}'.format(mid))
+            count+=1
+            print count 
+
+    sql = 'select id,name from country'
+    count=0
+    for mid,name in query_op.query_database(sql):
+        if name not in msas:
+            del_op.execute_sql('delete from datapiece where country_id={:}'.format(mid))
+            count+=1
+            print count 
+
+if __name__ == '__main__':
+    del_data()
+
--- util.py
+++ util.py
@@ -175,7 +175,7 @@ def test_cols(path):
     for line in open(path):
         line = line.strip()
         length= len(line.split("\t"))
-        if length!=196:
+        if length==196:
             print line
         # if len(line.split("\t"))!=96:
         #     print 'false'
